f5ab17bd91f4f16f4ca0ac514e94e754bf6d7187
gbd850 <mmyszaki@gmail.com>
2024-01-13 17:11:00 +0100

    Burger style nav + 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { RoomsComponent } from './rooms/rooms.component';
 import { RoomListComponent } from './rooms/room-list/room-list.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 export const routes: Routes = [
   { path: 'rooms', component: RoomsComponent },
   { path: 'roomlist', component: RoomListComponent },
   { path: '', redirectTo: '/rooms', pathMatch: 'full' },
+  { path: '**', component: NotfoundComponent },
 ];
